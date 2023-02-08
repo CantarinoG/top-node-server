23d422bf23b9db379bc7d4db28ae993a00946b9c
CantarinoG <guilhermecantarino1709@gmail.com>
2023-02-08 13:59:16 -0300

    Add .css fetching

--- index.js
+++ index.js
@@ -7,22 +7,44 @@ http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
 
-  if (filename === "./") filename = "./index";
+  if(filename === "./style.css") {
 
-  if(!fs.existsSync(filename + ".html")) filename = "./404";
+    fs.readFile(filename, function (err, data){
+        
+        if (err) {
 
-  fs.readFile(filename + ".html", function(err, data) {
+            res.writeHead(404);
+            return res.end();
 
-    if (err) {
+        }
 
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        return res.end(data);
 
-    } 
+    });
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
+  }
+ 
+  else {
 
-  });
+    if (filename === "./") filename = "./index";
+
+    if(!fs.existsSync(filename + ".html")) filename = "./404";
+
+    fs.readFile(filename + ".html", function(err, data) {
+
+        if (err) {
+
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+
+        } 
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+
+    });
+
+  }
 }).listen(8080);
